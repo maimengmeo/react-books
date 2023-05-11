dcb8a97270a336aa912787e4e5c93ea48f12eb4b
maimengmeo <mai291294@gmail.com>
2023-05-11 19:42:51 -0400

    Receive new title - communicate app - bookcreate with prop onCreate - handle title change and submit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-    return <div>App</div>;
+    const [books, setBooks] = useState([]);
+
+    const createBook = async (title) => {
+        console.log("Book title: ", title);
+    };
+
+    return (
+        <div>
+            <BookCreate onCreate={createBook} />
+        </div>
+    );
 }
 
 export default App;
